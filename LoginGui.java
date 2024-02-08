c682dcc3cd2837833cee58a2ddefb600bcda9e5f
Anne Apiyo <93118744+1287Apiyo@users.noreply.github.com>
2024-02-08 19:51:42 +0300

    Add files via upload

--- LoginGui.java
+++ LoginGui.java
@@ -1,2 +1,13 @@
-public class LoginGui {
+import javax.swing.*;
+
+public class LoginGui  extends JFrame {
+ public LoginGui(){
+     super("Apiyo Ind. Login");
+     setSize(CommonConstraints.FRAME_SIZE);
+     setLocationRelativeTo(null);
+     setResizable(false);
+     setLayout(null);
+
+ }
+
 }
