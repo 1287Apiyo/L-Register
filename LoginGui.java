de5b2a6982d512fded909debb2b11c2aee289173
Anne Apiyo <anneapiyo0@gmail.com>
2024-02-15 23:44:23 +0300

    Add files via upload

--- LoginGui.java
+++ LoginGui.java
@@ -5,9 +5,17 @@ public class LoginGui  extends JFrame {
      super("Apiyo Ind. Login");
      setSize(CommonConstraints.FRAME_SIZE);
      setLocationRelativeTo(null);
+     setDefaultCloseOperation(EXIT_ON_CLOSE);
      setResizable(false);
      setLayout(null);
 
+     addGuiComponents();
+
+ }
+ private void addGuiComponents(){
+     //login image
+     JLabel loginImage=CustomTools.loadImage(CommonConstraints.LOGIN_IMAGE_PATH);
+     loginImage.setBounds();
  }
 
 }
