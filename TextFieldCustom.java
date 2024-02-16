5a6d6e83bd6c16004f7127f3e92df7637d94f3fd
Anne Apiyo <anneapiyo0@gmail.com>
2024-02-16 16:08:17 +0300

    Add files via upload

--- TextFieldCustom.java
+++ TextFieldCustom.java
@@ -1,3 +1,18 @@
 public class TextFieldCustom {
-    private
+    private String placeholderText;
+    private boolean hasPlaceHolder;
+    public boolean isHasPlaceHolder() {
+        return hasPlaceHolder;
+    }
+
+    public TextFieldCustom(String placeholderText, int charLimit){
+        super();
+        this.placeholderText=placeholderText;
+
+        //placeholder text status
+        hasPlaceHolder=true;
+
+        //limit char input in field
+
+    }
 }
