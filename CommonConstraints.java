ce042c8d03807ad78c2879c25cbf073969f2b38d
Anne Apiyo <anneapiyo0@gmail.com>
2024-02-16 16:46:36 +0300

    Add files via upload

--- CommonConstraints.java
+++ CommonConstraints.java
@@ -1,10 +1,12 @@
 import java.awt.*;
 
 public class CommonConstraints {
-    public static final String LOGIN_IMAGE_PATH="resources/portfolio.png";
+    public static final String LOGIN_IMAGE_PATH="src/resources/portfolio.png";
 
     //frame configuration
     public static final Dimension FRAME_SIZE= new Dimension(540,760);
+    public static final Dimension TEXTFIELD_SIZE= new Dimension((int) (FRAME_SIZE.width*0.80),50);
+
     //login configuration
     public static final Dimension LOGIN_IMAGE_SIZE= new Dimension(255,262);
 
--- LoginGui.java
+++ LoginGui.java
@@ -1,6 +1,9 @@
+import custom.TextFieldCustom;
+
 import javax.swing.*;
 
 public class LoginGui  extends JFrame {
+    private TextFieldCustom usernameField;
  public LoginGui(){
      super("Apiyo Ind. Login");
      setSize(CommonConstraints.FRAME_SIZE);
@@ -15,11 +18,17 @@ public class LoginGui  extends JFrame {
  private void addGuiComponents(){
      //login image
      JLabel loginImage=CustomTools.loadImage(CommonConstraints.LOGIN_IMAGE_PATH);
+     assert loginImage != null;
      loginImage.setBounds((CommonConstraints.FRAME_SIZE.width - loginImage.getPreferredSize().width)/2,
      25, CommonConstraints.LOGIN_IMAGE_SIZE.width,CommonConstraints.LOGIN_IMAGE_SIZE.height
              );
      //Username field
-
+usernameField=new TextFieldCustom("Enter Username", 30);
+usernameField.setBounds(50,loginImage.getY() + 315,CommonConstraints.TEXTFIELD_SIZE.width,CommonConstraints.TEXTFIELD_SIZE.height
+);
+//add to frame
+     getContentPane().add(loginImage);
+     getContentPane().add(usernameField);
  }
 
 }
