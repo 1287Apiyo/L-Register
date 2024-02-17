f81aeac495e5a520205e18b27aa36e720e73d48b
Anne Apiyo <anneapiyo0@gmail.com>
2024-02-17 18:25:50 +0300

    Add files via upload

--- RegisterGui.java
+++ RegisterGui.java
@@ -4,8 +4,14 @@ import custom.TextFieldCustom;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class RegisterGui  extends JFrame {
+public class RegisterGui  extends JFrame implements FocusListener{
     private ErrorLabel usernameErrorLabel,passwordErrorLabel,confirmPasswordErrorLabel,emailErrorLabel;
     private TextFieldCustom usernameField,emailField;
     private PasswordFieldCustom passwordField,confirmPasswordField;
@@ -40,6 +46,8 @@ public class RegisterGui  extends JFrame {
                 50,
                 registerLabel.getY() + 150,
                 CommonConstraints.TEXTFIELD_SIZE.width, CommonConstraints.TEXTFIELD_SIZE.height);
+
+        usernameField.addFocusListener(this);
         //username error label
         usernameErrorLabel=new ErrorLabel("Invalid: Cannot be less than 6 characters");
         usernameErrorLabel.setBounds(50,usernameField.getY() + 50,
@@ -53,6 +61,10 @@ public class RegisterGui  extends JFrame {
                 usernameField.getY() + 100,
                 CommonConstraints.TEXTFIELD_SIZE.width, CommonConstraints.TEXTFIELD_SIZE.height
                 );
+
+        passwordField.addFocusListener(this);
+
+
         //password error label
         passwordErrorLabel=new ErrorLabel("*Invalid: Size > 6,1 Upper and Lower ,1 #, and 1 Special Char");
         passwordErrorLabel.setBounds(
@@ -67,7 +79,9 @@ public class RegisterGui  extends JFrame {
                 passwordField.getY() + 100,
                 CommonConstraints.TEXTFIELD_SIZE.width, CommonConstraints.TEXTFIELD_SIZE.height);
 
-        //confirm error password
+        confirmPasswordField.addFocusListener(this);
+
+        //confirm password error label
         confirmPasswordErrorLabel=new ErrorLabel("*Invalid:Password do not match");
         confirmPasswordErrorLabel.setBounds(
                 50,confirmPasswordField.getY() + 50,
@@ -83,6 +97,7 @@ public class RegisterGui  extends JFrame {
                 CommonConstraints.TEXTFIELD_SIZE.width, CommonConstraints.TEXTFIELD_SIZE.height);
 
 
+        emailField.addFocusListener(this);
         //email error label
         emailErrorLabel=new ErrorLabel("*Invalid:Not a valid format");
         emailErrorLabel.setBounds(
@@ -127,5 +142,46 @@ public class RegisterGui  extends JFrame {
 
         getContentPane().add(registerButton);
         getContentPane().add(loginLabel);
+    }
+
+    @Override
+    public void focusGained(FocusEvent e) {
+
+    }
+
+    @Override
+    public void focusLost(FocusEvent e) {
+        Object fieldSource = e.getSource();
+        if (fieldSource == usernameField) {
+            //valid username has to be greater or equal to 6
+            if (usernameField.getText().length() < 6 || usernameField.isHasPlaceHolder()) {
+                usernameErrorLabel.setVisible(true);
+            } else {
+                usernameErrorLabel.setVisible(false);
+            }
+        } else if (fieldSource == passwordField){
+            String passwordRegex="^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\\\S+$).{8, 20}$";
+            Pattern p =Pattern.compile(passwordRegex);
+            Matcher m=p.matcher(passwordField.getText());
+            if (!m.find())passwordErrorLabel.setVisible(true);
+            else passwordErrorLabel.setVisible(false);
+        } else if (fieldSource==confirmPasswordField) {
+            //if password does not match it is invalid
+            if (!passwordField.getText().equals(confirmPasswordField.getText())){
+                confirmPasswordErrorLabel.setVisible(true);
+            }else{
+                confirmPasswordErrorLabel.setVisible(false);
+            }
+            
+        }else if (fieldSource==emailField){
+            //checks validity of the email
+            String emailRegex=" ^[A-Za-z0-9+_.-]+@(.+)$";
+            Pattern p= Pattern.compile(emailRegex);
+            Matcher m = p.matcher(emailField.getText());
+            if (!m.find())emailErrorLabel.setVisible(true);
+            else emailErrorLabel.setVisible(false);
+        }
+
+
     }
 }
