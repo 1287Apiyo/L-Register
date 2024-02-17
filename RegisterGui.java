391b6dd396041b9396b0a77bf1d3968a3d01b062
Anne Apiyo <anneapiyo0@gmail.com>
2024-02-17 13:27:56 +0300

    Add files via upload

---
+++ RegisterGui.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class RegisterGui  extends JFrame {
+    public  RegisterGui(){
+        super();
+
+        setSize(CommonConstraints.FRAME_SIZE);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+        setLayout(null);
+
+    }
+}
