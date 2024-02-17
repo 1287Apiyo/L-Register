f5adfba061f642ea56ef34c66a402f6804243991
Anne Apiyo <anneapiyo0@gmail.com>
2024-02-17 15:23:37 +0300

    Add files via upload

--- RegisterGui.java
+++ RegisterGui.java
@@ -1,3 +1,4 @@
+import custom.ErrorLabel;
 import custom.PasswordFieldCustom;
 import custom.TextFieldCustom;
 
@@ -5,6 +6,7 @@ import javax.swing.*;
 import java.awt.*;
 
 public class RegisterGui  extends JFrame {
+    private ErrorLabel usernameErrorLabel,passwordErrorLabel,confirmPasswordErrorLabel,emailErrorLabel;
     private TextFieldCustom usernameField,emailField;
     private PasswordFieldCustom passwordField,confirmPasswordField;
     public  RegisterGui(){
@@ -38,6 +40,12 @@ public class RegisterGui  extends JFrame {
                 50,
                 registerLabel.getY() + 150,
                 CommonConstraints.TEXTFIELD_SIZE.width, CommonConstraints.TEXTFIELD_SIZE.height);
+        //username error label
+        usernameErrorLabel=new ErrorLabel("Invalid: Cannot be less than 6 characters");
+        usernameErrorLabel.setBounds(50,usernameField.getY() + 50,
+                CommonConstraints.TEXTFIELD_SIZE.width, CommonConstraints.TEXTFIELD_SIZE.height
+                );
+
 
         //password field
         passwordField=new PasswordFieldCustom("Enter Password",30);
@@ -45,22 +53,79 @@ public class RegisterGui  extends JFrame {
                 usernameField.getY() + 100,
                 CommonConstraints.TEXTFIELD_SIZE.width, CommonConstraints.TEXTFIELD_SIZE.height
                 );
-        //configure the password field
+        //password error label
+        passwordErrorLabel=new ErrorLabel("*Invalid: Size > 6,1 Upper and Lower ,1 #, and 1 Special Char");
+        passwordErrorLabel.setBounds(
+                50,passwordField.getY() + 50,
+                CommonConstraints.TEXTFIELD_SIZE.width, CommonConstraints.TEXTFIELD_SIZE.height
+        );
+
+        //confirm the password field
         confirmPasswordField=new PasswordFieldCustom("Confirm Password",30);
         confirmPasswordField.setBounds(
                 50,
                 passwordField.getY() + 100,
                 CommonConstraints.TEXTFIELD_SIZE.width, CommonConstraints.TEXTFIELD_SIZE.height);
+
+        //confirm error password
+        confirmPasswordErrorLabel=new ErrorLabel("*Invalid:Password do not match");
+        confirmPasswordErrorLabel.setBounds(
+                50,confirmPasswordField.getY() + 50,
+                CommonConstraints.TEXTFIELD_SIZE.width, CommonConstraints.TEXTFIELD_SIZE.height
+        );
+
+
        //email field
         emailField=new TextFieldCustom("Enter E-Mail",30);
         emailField.setBounds(
                 50,
                 confirmPasswordField.getY() + 100,
                 CommonConstraints.TEXTFIELD_SIZE.width, CommonConstraints.TEXTFIELD_SIZE.height);
-     //add to frame
+
+
+        //email error label
+        emailErrorLabel=new ErrorLabel("*Invalid:Not a valid format");
+        emailErrorLabel.setBounds(
+                50,emailField.getY() + 50,
+                CommonConstraints.TEXTFIELD_SIZE.width, CommonConstraints.TEXTFIELD_SIZE.height
+        );
+
+
+        //register button
+        JButton registerButton=new JButton("Register");
+        registerButton.setBounds(
+                50,
+                emailField.getY() + 100,
+                CommonConstraints.TEXTFIELD_SIZE.width,CommonConstraints.TEXTFIELD_SIZE.height
+        );
+
+        //register->login label
+        JLabel loginLabel=new JLabel("Already a user?Login Here!");
+        loginLabel.setBounds(
+                (CommonConstraints.FRAME_SIZE.width - loginLabel.getPreferredSize().width)/2,
+                registerButton.getY() + 100,
+                loginLabel.getPreferredSize().width,loginLabel.getPreferredSize().height
+        );
+
+        //add to frame
+        getContentPane().add(registerLabel);
+        //username
         getContentPane().add(usernameField);
+        getContentPane().add(usernameErrorLabel);
+
+        //password
         getContentPane().add(passwordField);
+        getContentPane().add(passwordErrorLabel);
+
+        //confirm password
         getContentPane().add(confirmPasswordField);
+        getContentPane().add(confirmPasswordErrorLabel);
+
+        //email
         getContentPane().add(emailField);
+        getContentPane().add(emailErrorLabel);
+
+        getContentPane().add(registerButton);
+        getContentPane().add(loginLabel);
     }
 }
